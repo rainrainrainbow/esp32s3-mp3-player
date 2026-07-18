e273c435a77985202ff2db36c8712f8b9ee2061f
rainrainrainbow <1448282415@qq.com>
2026-07-18 21:22:46 +0800

    update header for BMP-only

--- main/image_decoder.h
+++ main/image_decoder.h
@@ -1,18 +1,10 @@
 #ifndef IMAGE_DECODER_H
 #define IMAGE_DECODER_H
-
 #include <stdint.h>
 #include <stddef.h>
+#include <stdbool.h>
 
-// Minimum JPEG decoder for ESP32-S3
-// Decodes JPEG to RGB565 for TFT display
-bool jpeg_decode_to_rgb565(const uint8_t *jpeg_data, size_t len,
-                           uint16_t *out_buf, size_t buf_size,
-                           uint16_t *width, uint16_t *height);
+bool decode_bmp_file(const char *filepath, uint16_t *width, uint16_t *height, uint16_t **pixels);
+void free_decoded_image(uint16_t *pixels);
 
-// BMP decoder (simple 24-bit BMP)
-bool bmp_decode_to_rgb565(const uint8_t *bmp_data, size_t len,
-                          uint16_t *out_buf, size_t buf_size,
-                          uint16_t *width, uint16_t *height);
-
-#endif // IMAGE_DECODER_H
+#endif
