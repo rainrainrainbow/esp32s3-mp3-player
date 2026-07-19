e595754364de99cf7ac5690c1acdb7dce7f6038d
rainrainrainbow <1448282415@qq.com>
2026-07-19 17:26:28 +0800

    minimal-player: add tft_draw_pixel

--- main/tft_driver.h
+++ main/tft_driver.h
@@ -9,6 +9,7 @@ void tft_set_backlight(uint8_t brightness);
 void tft_fill_screen(uint16_t color);
 void tft_draw_jpeg(const uint8_t *jpeg_data, size_t len, uint16_t x, uint16_t y);
 void tft_draw_bmp(const uint8_t *bmp_data, size_t len);
+void tft_draw_pixel(uint16_t x, uint16_t y, uint16_t color);
 void tft_show_image_file(const char *filepath);
 
 #endif // TFT_DRIVER_H
