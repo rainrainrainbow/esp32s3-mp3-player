717a6a1f59f110165416e0380f6ec042ede40b92
rainrainrainbow <1448282415@qq.com>
2026-07-23 00:30:08 +0800

    fix: expose manual MSC ownership state

--- main/usb_msc.h
+++ main/usb_msc.h
@@ -1,8 +1,11 @@
 #ifndef USB_MSC_H
 #define USB_MSC_H
 
+#include <stdbool.h>
+
 void usb_msc_init(void);
-void usb_msc_switch_to_usb(void);
-void usb_msc_switch_to_app(void);
+bool usb_msc_switch_to_usb(void);
+bool usb_msc_switch_to_app(void);
+bool usb_msc_is_app_mode(void);
 
 #endif
