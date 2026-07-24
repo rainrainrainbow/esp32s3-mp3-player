e4cc31bf1a46c8090dfd390a1c903c3c204a2293
rainrainrainbow <1448282415@qq.com>
2026-07-24 18:28:59 +0800

    Update TFT driver header for new image decoder API

--- main/tft_driver.h
+++ main/tft_driver.h
@@ -7,9 +7,10 @@
 void tft_init(void);
 void tft_set_backlight(uint8_t brightness);
 void tft_fill_screen(uint16_t color);
-void tft_draw_jpeg(const uint8_t *jpeg_data, size_t len, uint16_t x, uint16_t y);
-void tft_draw_bmp(const uint8_t *bmp_data, size_t len);
 void tft_draw_pixel(uint16_t x, uint16_t y, uint16_t color);
 void tft_show_image_file(const char *filepath);
+
+// Backlight control with PWM-like levels (0-100)
+void tft_set_brightness(uint8_t percent);
 
 #endif // TFT_DRIVER_H
